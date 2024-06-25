bec072279cbe4d05a627c9a7ce4a985e295f5654
DESKTOP-5Q9K2NL\user1 <chay6865@gmail.com>
2024-06-25 16:13:40 +0300

    controller 2

---
+++ tryPraktikum/Controllers/successController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tryPraktikum.Controllers
+{
+    public class successController : Controller
+    {
+        // GET: successController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: successController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: successController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: successController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: successController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: successController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: successController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: successController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
